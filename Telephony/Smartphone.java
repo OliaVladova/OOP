e247086a4f0f6f4a22acfddaa046f5ac02df122e
OliaVladova <ovladova1@gmail.com>
2022-05-29 19:33:46 +0300

    Add files via upload

---
+++ Telephony/Smartphone.java
@@ -0,0 +1,42 @@
+package InterfacesAndAbstraction.Telephony;
+
+
+import java.util.List;
+
+public class Smartphone implements Callable, Browsable {
+    private List<String> numbers;
+    private List<String> urls;
+
+    public Smartphone(List<String> numbers, List<String> urls) {
+        this.numbers = numbers;
+        this.urls = urls;
+    }
+
+    @Override
+    public String browse() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String url : this.urls) {
+            if (url.matches("^[\\D]+$")) {
+                sb.append(String.format("Browsing: %s!", url)).append(System.lineSeparator());
+            } else {
+                sb.append("Invalid URL!").append(System.lineSeparator());
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String call() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String number : this.numbers) {
+            if (number.matches("^[\\d]+$")) {
+                sb.append(String.format("Calling... %s", number)).append(System.lineSeparator());
+            } else {
+                sb.append("Invalid number!").append(System.lineSeparator());
+            }
+        }
+        return sb.toString();
+    }
+}
