dff16d49bd08f193199e156b909f7a77803a73f1
OliaVladova <ovladova1@gmail.com>
2022-05-31 23:23:38 +0300

    Add files via upload

---
+++ Polymorphism/Vehicles/Bus.java
@@ -0,0 +1,8 @@
+package Polymorphism.Vehicles;
+
+public class Bus extends BaseVehicle {
+    public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption,tankCapacity);
+    }
+
+}
